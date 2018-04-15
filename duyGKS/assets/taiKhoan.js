ef85849db0e1c1f9370cf30c205e225adf74413a
tiendung2013vn97 <tiendung2013vn97@gmail.com>
2018-04-15 08:52:51 +0700

    Lam giao dien dang nhap va dang ki (chua hoan tat)

---
+++ duyGKS/assets/taiKhoan.js
@@ -0,0 +1,27 @@
+
+
+$("#dangKi").click(function(){
+$("#dangNhapContent").css({"display":"none"});
+$("#dangNhap").css({"background-color":"#FFFFFF","color":"black"});
+$("#modalForm").css({"height":"400px","width":"700px"});
+$(this).css({"color":"red","background-color":"#F1F1F1","padding-top":"7px","padding-bottom":"3px","padding-right":"20px"});
+$("#dangKiContent").css({"display":"block"});
+
+});
+
+$("#dangNhap").click(function(){
+	$("#dangKi").css({"background-color":"#FFFFFF","color":"black"});
+	$("#dangKiContent").css({"display":"none"});
+	$("#dangNhapContent").css({"display":"block"});
+	$("#modalForm").css({"height":"300px","width":"400px"});
+	$(this).css({"background-color":"#F1F1F1","color":"green"});
+});
+
+$("#taiKhoan").click(function(){
+$("#modalForm").css({"display":"block"});
+});
+
+$("#closeTaiKhoan").on('click',function(){
+	
+$("#modalForm").css({"display":"none"});
+});
