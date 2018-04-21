e5ae95bc668afdb962893d4b02b5dab8c8acd75d
tiendung2013vn97 <tiendung2013vn97@gmail.com>
2018-04-21 22:20:24 +0700

    làm giao diện thông tin chi tiết sản phẩm (70%)

---
+++ DAGK/assets/thongTinChiTietSanPham.js
@@ -0,0 +1,48 @@
+$(".slide-image img").click(function() {
+    var image_html = $(this).attr("src");
+    $(".sample-image img").attr("src", image_html);
+    $(this).addClass("border-red-visited");
+    var n = $(this).index();
+    $(".slide-image img").each(function() {
+        if ($(this).index() !== n) {
+            $(this).removeClass("border-red-visited");
+        }
+    });
+
+});
+
+$(".luotThich").click(function() {
+    var number = parseInt($(".luotThich span").text());
+    number++;
+    $(".luotThich span").text("" + number);
+});
+$(".soLuong button").click(function() {
+    var number = parseInt($(".soLuong span").text());
+    if ($(this).text() === '+') {
+        number++;
+    }
+    if ($(this).text() == '-') {
+        if (number > 0) { number--; }
+    }
+    $(".soLuong span").text('' + number);
+});
+$(".menu-dgsp").css('border-bottom', '2px solid white');
+$(".menu-dgsp").css('color', 'black');
+$("#danhGiaSanPham").css('display','none');
+$(".menu-ttct").click(function() {
+    $(".menu-ttct").css('border-bottom', '2px solid #FF5722');
+    $(".menu-ttct").css('color', '#FF5722');
+    $(".menu-dgsp").css('border-bottom', '2px solid white');
+    $(".menu-dgsp").css('color', 'black');
+    $("#danhGiaSanPham").css('display','none');
+    $("#thongTinChiTiet").css('display','block');
+
+});
+$(".menu-dgsp").click(function() {
+    $(".menu-dgsp").css('border-bottom', '2px solid #FF5722');
+    $(".menu-dgsp").css('color', '#FF5722');
+    $(".menu-ttct").css('border-bottom', '2px solid white');
+    $(".menu-ttct").css('color', 'black');
+        $("#danhGiaSanPham").css('display','block');
+    $("#thongTinChiTiet").css('display','none');
+});
