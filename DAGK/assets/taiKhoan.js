553921ddee9de4840d798a325cc92ad14b931ae7
tiendung2013vn97 <tiendung2013vn97@gmail.com>
2018-04-26 00:19:16 +0700

    Chỉnh xong modal form đăng kí, đăng nhập

--- DAGK/assets/taiKhoan.js
+++ DAGK/assets/taiKhoan.js
@@ -1,37 +1,40 @@
+$(document).ready(function() {
+	$("#modalForm").click(function(e){
+		e.stopPropagation();
+	});
+    $("#dangKi").click(function() {
+        $("#dangNhapContent").css({ "display": "none" });
+        $("#dangNhap").css({ "background-color": "#FFFFFF", "color": "black" });
+        $("#modalForm").css({ "height": "470px", "width": "700px" });
+        $(this).css({ "color": "red", "background-color": "#F1F1F1", "padding-top": "7px", "padding-bottom": "3px", "padding-right": "20px" });
+        $("#dangKiContent").css({ "display": "block" });
+       
+    });
 
-$(document).ready(function(){
+    $("#dangNhap").click(function() {
+        $("#dangKi").css({ "background-color": "#FFFFFF", "color": "black" });
+        $("#dangKiContent").css({ "display": "none" });
+        $("#dangNhapContent").css({ "display": "block" });
+        $("#modalForm").css({ "height": "280px", "width": "400px" });
+        $(this).css({ "background-color": "#F1F1F1", "color": "green" });
+        
+    });
 
-$("#dangKi").click(function(){
-$("#dangNhapContent").css({"display":"none"});
-$("#dangNhap").css({"background-color":"#FFFFFF","color":"black"});
-$("#modalForm").css({"height":"470px","width":"700px"});
-$(this).css({"color":"red","background-color":"#F1F1F1","padding-top":"7px","padding-bottom":"3px","padding-right":"20px"});
-$("#dangKiContent").css({"display":"block"});
+    $("#taiKhoan").click(function() {
+        $(".wrapModalForm").css({ "display": "block" });
+        $("#modalForm form")[0].reset();
+        $("#modalForm form")[1].reset();
+        $("#dangKi").css({ "background-color": "#FFFFFF", "color": "black" });
+        $("#dangKiContent").css({ "display": "none" });
+        $("#dangNhapContent").css({ "display": "block" });
+        $("#modalForm").css({ "height": "280px", "width": "400px" });
+        $("#dangNhap").css({ "background-color": "#F1F1F1", "color": "green" });
+    });
 
-});
-
-$("#dangNhap").click(function(){
-	$("#dangKi").css({"background-color":"#FFFFFF","color":"black"});
-	$("#dangKiContent").css({"display":"none"});
-	$("#dangNhapContent").css({"display":"block"});
-	$("#modalForm").css({"height":"280px","width":"400px"});
-	$(this).css({"background-color":"#F1F1F1","color":"green"});
-});
-
-$("#taiKhoan").click(function(){
-$("#modalForm").css({"display":"block"});
-$("#modalForm form")[0].reset();
-$("#modalForm form")[1].reset();
-	$("#dangKi").css({"background-color":"#FFFFFF","color":"black"});
-	$("#dangKiContent").css({"display":"none"});
-	$("#dangNhapContent").css({"display":"block"});
-	$("#modalForm").css({"height":"280px","width":"400px"});
-	$("#dangNhap").css({"background-color":"#F1F1F1","color":"green"});
-});
-
-$("#closeTaiKhoan").on('click',function(){
-	
-$(this).closest("#modalForm").css({"display":"none"});
-});
-
+    $("#closeTaiKhoan").on('click', function() {
+        $(this).closest(".wrapModalForm").css({ "display": "none" });
+    });
+    $(".wrapModalForm").click(function() {
+        $(".wrapModalForm").css({ "display": "none" });
+    });
 });
