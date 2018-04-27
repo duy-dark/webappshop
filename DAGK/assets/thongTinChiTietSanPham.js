d223205ebfc5decf26710a041f6cf107be4017e7
tiendung2013vn97 <tiendung2013vn97@gmail.com>
2018-04-27 15:19:46 +0700

    hoàn tất giao diện thông tin chi tiết sản phẩm

--- DAGK/assets/thongTinChiTietSanPham.js
+++ DAGK/assets/thongTinChiTietSanPham.js
@@ -28,14 +28,14 @@ $(".soLuong button").click(function() {
 });
 $(".menu-dgsp").css('border-bottom', '2px solid white');
 $(".menu-dgsp").css('color', 'black');
-$("#danhGiaSanPham").css('display','none');
+$("#danhGiaSanPham").css('display', 'none');
 $(".menu-ttct").click(function() {
     $(".menu-ttct").css('border-bottom', '2px solid #FF5722');
     $(".menu-ttct").css('color', '#FF5722');
     $(".menu-dgsp").css('border-bottom', '2px solid white');
     $(".menu-dgsp").css('color', 'black');
-    $("#danhGiaSanPham").css('display','none');
-    $("#thongTinChiTiet").css('display','block');
+    $("#danhGiaSanPham").css('display', 'none');
+    $("#thongTinChiTiet").css('display', 'block');
 
 });
 $(".menu-dgsp").click(function() {
@@ -43,14 +43,25 @@ $(".menu-dgsp").click(function() {
     $(".menu-dgsp").css('color', '#FF5722');
     $(".menu-ttct").css('border-bottom', '2px solid white');
     $(".menu-ttct").css('color', 'black');
-        $("#danhGiaSanPham").css('display','block');
-    $("#thongTinChiTiet").css('display','none');
+    $("#danhGiaSanPham").css('display', 'block');
+    $("#thongTinChiTiet").css('display', 'none');
 });
-$(".luotThich").hover(function(){
-$(".luotThich img").attr('src','assets/image/heart2.png');
-$(".luotThich").css({"color":"red"});
-},function(){
-$(".luotThich img").attr('src','assets/image/heart.png');
-$(".luotThich").css({"color":"black"});
-}
-);
+$(".luotThich").hover(function() {
+    $(".luotThich img").attr('src', 'assets/image/heart2.png');
+    $(".luotThich").css({ "color": "red" });
+}, function() {
+    $(".luotThich img").attr('src', 'assets/image/heart.png');
+    $(".luotThich").css({ "color": "black" });
+});
+$(window).scroll(function(){
+ if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        $("#goTop").css({"display":"block"});
+    } else {
+        $("#goTop").css({"display":"none"});
+    }
+});
+
+$("#goTop").click(function(){
+document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
