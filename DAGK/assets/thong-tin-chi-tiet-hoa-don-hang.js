899ed5b106d62770bd322d43e5d04facf141b760
tiendung2013vn97 <tiendung2013vn97@gmail.com>
2018-04-28 20:12:33 +0700

    Thêm tính năng thay đổi tình trạng đơn hàng

---
+++ DAGK/assets/thong-tin-chi-tiet-hoa-don-hang.js
@@ -0,0 +1,17 @@
+$("#ttdh button").click(function(){
+	var value=$("#ttdh").find("select").val();
+	value=parseInt(value);
+	if(value===0){
+
+		$('#ttdh>span').attr('class','chuaGiaoHang');
+		$('#ttdh>span').html('Chưa giao hàng');
+	}
+	if(value===1){
+		$('#ttdh>span').attr('class','dangGiaoHang');
+		$('#ttdh>span').html('Đang giao hàng');
+	}
+	if(value===2){
+		$('#ttdh>span').attr('class','daGiaoHang');
+		$('#ttdh>span').html('Đã giao hàng');
+	}
+})
