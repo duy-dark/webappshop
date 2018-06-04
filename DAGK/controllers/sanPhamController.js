f98f7be6487eac2536f9812d42d3f7149ef1c32a
ducmonkeyd <ducmonkeyd@gmail.com>
2018-06-04 10:00:46 +0700

    update

--- DAGK/controllers/sanPhamController.js
+++ DAGK/controllers/sanPhamController.js
@@ -1,28 +1,7 @@
-var express = require('express'), 
-	product = require('../repos/productRepo');
+var express = require('express'),
+    categoryRepo = require('../repos/categoryRepo');
 
 var router = express.Router();
-
-router.get('/sanpham/san-pham-theo-hang', (req, res) => {
-    res.render('sanPham/San-pham-theo-hang');
-    var p1 = productRepo.load(NSX);
-    var x = req.query.value
-    Promise.all	([p1]).then(([rows, count_rows]) => {
-    	var numbers = [];
-        for (i = 1; i <= nPages; i++) {
-            numbers.push({
-                value: i,
-                isCurrentPage: i === +page
-            });
-        }
-        var vm = {
-            products: rows,
-            noProducts: rows.length === 0,
-            page_numbers: numbers
-        };
-        res.render('/sanPham/San-pham-theo-hang', vm);
-    });
-});
 
 router.get('/thong-tin-chi-tiet-san-pham', (req, res) => {
     categoryRepo.loadid(+req.query.id).then(rows =>{
@@ -33,4 +12,6 @@ router.get('/thong-tin-chi-tiet-san-pham', (req, res) => {
     });
 });
 
+
 module.exports = router;
+
