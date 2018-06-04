076a0d2c065fd112bbdc71a5a68eb0bec1fd5ae0
ducmonkeyd <ducmonkeyd@gmail.com>
2018-06-04 09:39:40 +0700

    update get.thongtinchitiet(PQDUY)

--- DAGK/controllers/sanPhamController.js
+++ DAGK/controllers/sanPhamController.js
@@ -24,4 +24,13 @@ router.get('/sanpham/san-pham-theo-hang', (req, res) => {
     });
 });
 
+router.get('/thong-tin-chi-tiet-san-pham', (req, res) => {
+    categoryRepo.loadid(+req.query.id).then(rows =>{
+        var vm={
+            sp:rows[0]
+        }
+        res.render('sanPham/thong-tin-chi-tiet-san-pham',vm);
+    });
+});
+
 module.exports = router;
