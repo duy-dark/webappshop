586f046e330073228f79a1f874fac45ed98270ff
ducmonkeyd <ducmonkeyd@gmail.com>
2018-06-04 10:08:50 +0700

    update

--- DAGK/controllers/sanPhamController.js
+++ DAGK/controllers/sanPhamController.js
@@ -3,6 +3,28 @@ var express = require('express'),
 
 var router = express.Router();
 
+
+router.get('/sanpham/san-pham-theo-hang', (req, res) => {
+    res.render('sanPham/San-pham-theo-hang');
+    var p1 = productRepo.load(NSX);
+    var x = req.query.value
+    Promise.all ([p1]).then(([rows, count_rows]) => {
+        var numbers = [];
+        for (i = 1; i <= nPages; i++) {
+            numbers.push({
+                value: i,
+                isCurrentPage: i === +page
+            });
+        }
+        var vm = {
+            products: rows,
+            noProducts: rows.length === 0,
+            page_numbers: numbers
+        };
+        res.render('/sanPham/San-pham-theo-hang', vm);
+    });
+});
+
 router.get('/thong-tin-chi-tiet-san-pham', (req, res) => {
     categoryRepo.loadid(+req.query.id).then(rows =>{
         var vm={
