7487bb92923079dfb1096b0ac702af6b31f787cb
ducmonkeyd <ducmonkeyd@gmail.com>
2018-06-05 13:51:50 +0700

    load data phan trang theo hang

--- DACK/repos/productRepo.js
+++ DACK/repos/productRepo.js
@@ -1,8 +1,12 @@
 var db = require('../fn/db');
+var config = require('../config/config');
 
-
-exports.load = (NSX) =>{
-	var sql = `select * from sanpham where NSX = ${NSX}`;
+exports.loadPageByCat = (catId, offset) => {
+	var sql = `select sp.* from sanpham sp, sanpham sp1 where sp.NSX=sp1.NSX and sp1.MASP = ${catId} limit ${config.PRODUCTS_PER_PAGE} offset ${offset}`;
 	return db.load(sql);
 }
 
+exports.countByCat = catId => {
+	var sql = `select count(*) as total from sanpham sp, sanpham sp1 where sp.NSX=sp1.NSX and sp1.MASP = ${catId} `;
+	return db.load(sql);
+}
