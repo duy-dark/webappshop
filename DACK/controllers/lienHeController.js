fd387656a20235b663042ad8fa905f06dc691619
duy-dark <36453006+duy-dark@users.noreply.github.com>
2018-06-27 22:57:54 +0700

    làm xong liên hệ

--- DACK/controllers/lienHeController.js
+++ DACK/controllers/lienHeController.js
@@ -1,10 +1,23 @@
 var express = require('express');
+var taiKhoanRepo = require('../repos/taiKhoanRepo');
 
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('lienHe/lien-he');
+	taiKhoanRepo.loadAdmin().then(rows=>{
+		var vm={
+			admin:rows[0]
+		};
+		res.render('lienHe/lien-he',vm);
+	});
+    
 });
-
+router.post('/send', (req, res) => {
+	console.log('1');
+	taiKhoanRepo.addmessage(req.body).then(rows=>{
+		res.redirect('/home');
+	});
+    
+});
 
 module.exports = router;
